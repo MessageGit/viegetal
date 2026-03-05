4b4049f081bb9ce1488bb32ffb918abe0e0eb67f
MessageBox <message-box@outlook.fr>
2026-03-05 14:05:05 +0100

    Edit next.config.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/viegetal',
+  assetPrefix: '/viegetal/',
+  trailingSlash: true,
+  images: { unoptimized: true },
   reactCompiler: true,
 };
 
